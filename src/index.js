4eab4ac1f859becb64b9c34e90e783f2c399cc2f
melilotbulge <kleslie1982@gmail.com>
2023-01-05 00:32:13 -0600

    Version final plus README

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@ console.log(validator);
 const clickValidation=document.getElementById("submitbutton");
 console.log(clickValidation);
 clickValidation.addEventListener("click", (e) =>{
-    e.preventDefault();
-    const cardNum=document.getElementById("cardNum").value;  
-    console.log(cardNum);
+  e.preventDefault();
+  const cardNum=document.getElementById("cardNum").value;  
+  console.log(cardNum);
 
-    const result= validator.isValid(cardNum);
-    console.log(validator.isValid(cardNum));
-
-if(cardNum.length<13 || cardNum.length>18 || result===false) 
-   {alert("Porfavor ingresa un numero de TARJETA VALIDO!!!!");
+  const result= validator.isValid(cardNum);
+  
+  console.log(validator.isValid(cardNum));
+ 
+  if(cardNum.length<13 || cardNum.length>18 || result===false) 
+  {alert("Porfavor ingresa un numero de TARJETA VALIDO!!!!");
     return}
-    else { alert(`Puedes continuar tu TARJETA ${validator.maskify(cardNum)} ES VALIDA`);
+  else { alert(`Puedes continuar tu TARJETA ${validator.maskify(cardNum)} ES VALIDA`);
     return}
    
 
--- src/validator.js
+++ src/validator.js
@@ -1,49 +1,50 @@
 const validator = {
 
 
-isValid: function (cardNum) {
+  isValid: function (cardNum) {
       
-      let addition = 0;
-      const cNumArr= Array.from(cardNum,Number);
+    let addition = 0;
+    const cNumArr= Array.from(cardNum,Number);
       
-      console.log(typeof(cNumArr))
+    console.log(typeof(cNumArr))
   
-      const cNumRev=cNumArr.reverse();
-      console.log(cNumRev);
-      console.log(typeof(cNumRev));
+    const cNumRev=cNumArr.reverse();
+    console.log(cNumRev);
+    console.log(typeof(cNumRev));
 
-      for (let i = 1; i < cNumRev.length; i +=2) {
-        let multiply = cNumRev[i]*2
-        if (multiply > 9) {
-          multiply -= 9
-        }
-        addition+=multiply
+    for (let i = 1; i < cNumRev.length; i +=2) {
+      let multiply = cNumRev[i]*2
+      if (multiply > 9) {
+        multiply -= 9
       }
-      for (let i = 0; i < cNumRev.length; i +=2) {
-       let dontMultiply = cNumRev[i];
-        addition+=dontMultiply
-  
-      }
-        console.log(addition) 
-        
-        return addition % 10 === 0
-},
-
-maskify: function (cardNum) {
-  const cNumArr = Array.from(cardNum, Number);
-  const size = cNumArr.length;
-  let rMask = "";
-  for (let i = 0; i <size; i++) {
-    if (i < size - 4) {
-      const mask = ("#");
-      rMask += mask;
-    } else {
-      const unMask = (cNumArr[i]);
-      rMask += unMask;
+      addition+=multiply
     }
+    for (let i = 0; i < cNumRev.length; i +=2) {
+      const dontMultiply = cNumRev[i];
+      addition+=dontMultiply
+  
+    }
+    console.log(addition) 
+        
+    return addition % 10 === 0
+  },
+
+  maskify: function (cardNum) {
+    const cNumArr = Array.from(cardNum, Number);
+    const size = cNumArr.length;
+    let rMask = "";
+    for (let i = 0; i <size; i++) {
+      if (i < size - 4) {
+        const mask = ("#");
+        rMask += mask;
+      } else {
+        const unMask = (cNumArr[i]);
+
+        rMask += unMask;
+      }
+    }
+    return rMask;
   }
-  return rMask;
-}
 
 }
 export default validator;
